807a9a575f30ffb882c0c8f88597709f4293ac60
Abdullah Topal <abdullahtopal0@gmail.com>
2024-03-10 20:13:55 +0300

    example.env

--- initializers/loadEnvVariables.go
+++ initializers/loadEnvVariables.go
@@ -7,8 +7,8 @@ import (
 )
 
 func LoadEnvVariables() {
-	err := godotenv.Load(".env")
+	err := godotenv.Load("example.env")
 	if err != nil {
-		log.Fatal("Error loading .env file")
+		log.Fatal("Error loading example.env file")
 	}
 }
